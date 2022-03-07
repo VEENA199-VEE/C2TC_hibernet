90a2f87ecfb30098d222a812ba8b060ee419de05
VEENA199-VEE <96383117+VEENA199-VEE@users.noreply.github.com>
2022-03-07 09:32:16 +0530

    First Commit

---
+++ JPAQueriesProject/src/com/tns/jpacrud/client/Client.java
@@ -0,0 +1,46 @@
+package com.tns.jpacrud.client;
+
+
+import com.tns.jpacrud.entities.Book;
+import com.tns.jpacrud.service.BookService;
+import com.tns.jpacrud.service.BookServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		BookService service = new BookServiceImpl();
+		
+		
+		System.out.println("************Listing total number of books*************");
+		System.out.println("Total books:"+service.getBookCount());
+		
+		System.out.println("************Listing book with id 105*************");
+		System.out.println("Boo with ID 1:"+service.getBookById(1));
+		
+		System.out.println("************Listing All books*************");
+		for(Book book:service.getAllBooks()) {
+			System.out.println(book);
+		}
+		
+		
+		
+		System.out.println("************Listing book written by Abdul Kalam*************");
+		for(Book book:service.getAuthorBooks("Abdul Kalam") ) {
+			System.out.println(book);
+		}
+		
+		System.out.println("************Listing book on Android*************");
+		for(Book book:service.getBookByTitle("The Complete Reference")) {
+			System.out.println(book);
+		}
+		
+		
+		
+		System.out.println("************Listing All books between 500 and 600*************");
+		for(Book book:service.getBooksInPriceRange(500, 600) ) {
+			System.out.println(book);
+		}		
+
+	}
+}
